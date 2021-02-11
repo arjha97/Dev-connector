9a9b170d20c019b69060267bb5b14f4872c902e5
Abhishek Ranjan <64737963+arjha97@users.noreply.github.com>
2021-02-11 16:52:30 +0530

    sixth commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
@@ -51,6 +52,7 @@ const App = () => {
              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
              <PrivateRoute exact path='/add-experience' component={AddExperience} />
              <PrivateRoute exact path='/add-education' component={AddEducation} />
+             <PrivateRoute exact path='/posts' component={Posts} />
            </Switch>
         </section>
       </Fragment>
