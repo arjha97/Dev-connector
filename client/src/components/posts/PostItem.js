3a5ce4065af4dda127e4814a044ca46ed9264029
Abhishek Ranjan <64737963+arjha97@users.noreply.github.com>
2021-02-15 13:21:45 +0530

    eight commit

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import {connect} from 'react-redux';
 import { addLike, removeLike, deletePost} from '../../actions/post';
 
-const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments, date}, addLike, removeLike, deletePost}) => (
+const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments, date}, addLike, removeLike, deletePost, showActions}) => (
   
      <div className="post bg-white p-1 my-1">
         <div>
@@ -25,22 +25,25 @@ const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments,
            <p className="post-date">
               Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
           </p>
-          <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+
+          {showActions && (
+           <Fragment>
+            <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
             <i className="fas fa-thumbs-up"></i>{' '}
             <span>{likes.length > 0 && (
             <span>{likes.length}</span>
             )}</span>
-          </button>
-          <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
-            <i className="fas fa-thumbs-down"></i>
-          </button>
-          <Link to={`/post/${_id}`} className="btn btn-primary">
-            Comments{' '} 
-            {comments.length > 0 && (
-            <span className='comment-count'>{comments.length}</span>
-            )}
-          </Link>
-          {!auth.loading && user === auth.user._id && (
+            </button>
+            <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+              <i className="fas fa-thumbs-down"></i>
+            </button>
+            <Link to={`/posts/${_id}`} className="btn btn-primary">
+              Comments{' '} 
+              {comments.length > 0 && (
+              <span className='comment-count'>{comments.length}</span>
+              )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
               <button 
                 onClick={e => deletePost(_id)}     
                 type="button"
@@ -48,10 +51,16 @@ const PostItem = ({ auth, post: {_id, text, name, avatar, user, likes, comments,
                 >
                 Delete
               </button>
-          )}
+            )}
+            </Fragment>
+            )}
         </div>
       </div>
     );
+
+PostItem.defaultProps = {
+      showActions: true
+};
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -2,7 +2,7 @@
 import {
     GET_POSTS,
     POST_ERROR,
-    ADD_POST, DELETE_POST , UPDATE_LIKES
+    ADD_POST, DELETE_POST , UPDATE_LIKES, GET_POST, ADD_COMMENT, REMOVE_COMMENT
 } from '../actions/types';
 
 
@@ -23,10 +23,16 @@ export default function(state = initialState, action) {
                 posts: payload,
                 loading: false
             }
+        case GET_POST:
+            return {
+                ...state,
+                post: payload,
+                loading: false
+            }    
         case ADD_POST:
             return {
                 ...state,
-                posts: {payload, ...state.posts},
+                posts: [payload, ...state.posts],
                 loading: false
             }    
         case DELETE_POST:
@@ -46,7 +52,22 @@ export default function(state = initialState, action) {
                 ...state,
                 posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: payload.likes} : post),
                 loading: false
-            }   
+            } 
+        case ADD_COMMENT:
+            return{
+                ...state,
+                post: {...state.post, comments: payload},
+                loading: false
+            }      
+        case REMOVE_COMMENT:
+            return{
+                ...state,
+                post: {
+                    ...state.post,
+                    comments: state.post.comments.filter(comment => comment._id !== payload)
+                },
+                loading: false
+            }    
         default:
             return state;      
     }
